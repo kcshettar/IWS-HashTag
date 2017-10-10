65cce5dae7ed37df22a511b1af506e838350f107
Kiran Shettar <kcshettar@gmail.com>
2017-10-10 18:40:53 -0400

    Recent

---
+++ IWSWebApplication2/IWSWebApplication/IWSWebApplication/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IWSWebApplication.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ IWSWebApplication2/IWSWebApplication/LoginBoard/src/app/web.service.ts
@@ -0,0 +1,13 @@
+// import { Http } from '@angular/http';
+// import {Injectable} from '@angular/core';
+// import 'rxjs/add/operator/toPromise';
+
+// @Injectable()
+// export class WebService {
+//     //constructor
+//     constructor(private http: Http) {}
+
+//     getMessages() {
+//         return this.http.get('http://localhost:50016/api/Messages').toPromise();
+//     }
+// }
