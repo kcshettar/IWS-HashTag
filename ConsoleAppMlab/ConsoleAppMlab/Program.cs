abfb79ba2f6fcb8fcbe5c795630bd2e409647cbd
Kiran Shettar <kcshettar@gmail.com>
2017-11-09 16:31:02 -0500

    mLab Connection App

---
+++ ConsoleAppMlab/ConsoleAppMlab/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using Json;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+namespace ConsoleAppMlab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CallMain(args).Wait();
+        }
+
+        static async Task CallMain(string[] args)
+        {
+            string connectionString = "mongodb://kshettar:Skiran*3@ds249325.mlab.com:49325/users";
+            var mongoClient = new MongoClient(connectionString);
+            var db = mongoClient.GetDatabase("users");
+            //string output = mongo.Client.WithReadConcern ;
+            //Console.WriteLine(output);
+            //var collection = db.GetCollection<BsonDocument>("userdata");
+            var collection = db.GetCollection<BsonDocument>("userdata");
+            var documnt = new BsonDocument
+            {
+                {"First Name","SSS"},
+                {"Last Name","KKK"},
+                {"State","USA"}
+            };
+            collection.InsertOneAsync(documnt);
+            Console.WriteLine("Press Enter to insert the data");
+            Console.ReadLine();
+
+
+            //var collection = db.GetCollection<BsonDocument>("userdata");
+            //await collection.Find(new BsonDocument()).ForEachAsync(X => Console.WriteLine(X));
+
+        }
+    }
+}
