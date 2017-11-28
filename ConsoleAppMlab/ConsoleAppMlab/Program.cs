e4f1634ad133204657e25bba8e39afa53611b636
Kiran Shettar <kcshettar@gmail.com>
2017-11-28 14:58:42 -0500

    Rest API Project + Mlab Code for Bson Deserializing

---
+++ ConsoleAppMlab/ConsoleAppMlab/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver.Core;
+using Json;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleAppMlab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CallMain(args).Wait();
+        }
+
+        static async Task CallMain(string[] args)
+        {
+            string connectionString = "mongodb://kshettar:Skiran*3@ds249325.mlab.com:49325/users";
+            var mongoClient = new MongoClient(connectionString);
+            var db = mongoClient.GetDatabase("users");
+            //string output = mongo.Client.WithReadConcern ;
+            //Console.WriteLine(output);
+            var collection = db.GetCollection<BsonDocument>("userdata");
+            //var collection = db.GetCollection<BsonDocument>("userdata");
+
+            //var documnt = new BsonDocument
+            //{
+            //    {"First Name","SSS"},
+            //    {"Last Name","KKK"},
+            //    {"State","USA"}
+            //};
+            //collection.InsertOneAsync(documnt);
+
+
+
+            Console.WriteLine("Press Enter to insert the data");
+            Console.ReadLine();
+
+
+
+            //var collection = db.GetCollection<BsonDocument>("userdata");
+            //await collection.Find(new BsonDocument()).ForEachAsync(X => Console.WriteLine(X));
+
+            var docs = collection.Find(new BsonDocument()).ToListAsync().GetAwaiter().GetResult();
+
+            var filter2 = Builders<BsonDocument>.Filter.Eq("First Name", "KCS");
+            var result = await collection.Find(filter2).ToListAsync();
+
+            foreach(BsonDocument l in docs)
+            {
+                var documnt = new BsonDocument(l);
+
+                string id = documnt[0].ToString();
+                string FirstName = documnt[1].ToString();
+                string LastName = documnt[2].ToString();
+                string Email = documnt[3].ToString();
+                Console.WriteLine(FirstName + "|" + LastName + "|" + Email + "\n");
+                
+            }
+
+            Console.WriteLine("Hello");
+            
+        }
+    }
+}
---
+++ WebApplicationRestApi/WebApplicationRestApi/Controllers/JobController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationRestApi.Model;
+
+//MongoDB
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace WebApplicationRestApi.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/[controller]")]
+    public class JobController : Controller
+    {
+
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private static string[] JobLocation = new[]
+        {
+            "Massachusetts", "California", "Atlanta", "Gearogia", "NewYork", "Florida", "Chicago", "Texas", "Georgia", "Utah"
+        };
+
+
+        [HttpGet("[action]")]
+        public IEnumerable<Job> GetAllJobs()
+        {
+            //var list = new List<Job>();
+            //list.Add(new Job() { Name = "Lilly" });
+            //list.Add(new Job() { Name = "Lucy" });
+            //return list;
+
+            //string connectionString = "mongodb://kshettar:Skiran*3@ds249325.mlab.com:49325/users";
+            //var mongoClient = new MongoClient(connectionString);
+            //var db = mongoClient.GetDatabase("users");
+            //var collection = db.GetCollection<BsonDocument>("userdata");
+
+            //var document = new BsonDocument
+            //{
+            //    {"First Name", user.firstName},
+            //    {"Last Name", user.lastName},
+            //    {"E-Mail", user.eMail},
+            //    {"Password", user.userPassword }
+            //};
+            //collection.InsertOneAsync(document);
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Job
+            {
+                Name = Summaries[rng.Next(Summaries.Length)],
+                Location = JobLocation[rng.Next(JobLocation.Length)]
+            });
+        }
+
+        //[HttpGet("{name}")]
+        //public Job Get(string name)
+        //{
+        //    return new Job() { Name = name };
+        //}
+
+        //[HttpPost]
+        //public Job Insert([FromBody]Model.Job job)
+        //{
+        //    // write the new cat to database
+        //    return job;
+        //}
+
+        //[HttpPut("{name}")]
+        //public Job Update(string name, [FromBody]Job job)
+        //{
+        //    job.Name = name;
+        //    // write the updated cat to database
+        //    return job;
+        //}
+
+        //[HttpDelete("{name}")]
+        //public void Delete(string name)
+        //{
+        //    // delete the cat from the database
+
+        //}
+    }
+}
